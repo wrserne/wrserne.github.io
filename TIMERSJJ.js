930acab733bf2603baadc283d549db9335244e30
wrserne <110362301+wrserne@users.noreply.github.com>
2022-11-23 17:17:22 -0500

    Add files via upload

---
+++ TIMERSJJ.js
@@ -0,0 +1,29 @@
+function countDown(time){
+    let timer = setInterval(function(){
+        time--;
+        if(time<=0){
+            clearInterval(timer);
+            console.log('DONE');
+        }
+        else{
+            console.log(time);
+        }
+    }, 1000)
+
+}
+
+function randomGame(){
+    let num;
+    let times = 0;
+    let timer = setInterval(function(){
+      num = Math.random();
+      times++
+      if(num > .75) {
+        clearInterval(timer);
+        console.log("It took " + times + " try/tries.");
+      }
+    },1000)
+  }
+
+  randomGame();
+
