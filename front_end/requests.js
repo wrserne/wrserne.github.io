d5987883bb94bbc643c7fee36c8224fb1ed3c94c
Serne Family <sernefamily@Sernes-MacBook-Air.local>
2023-02-18 21:39:32 -0500

    updated request file

--- front_end/requests.js
+++ front_end/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://your-reef-pi-api-url/';
+const baseURL = 'http://your-reef-pi-api-url/'; // this should be my rasberry pi IP on local network
 const apiKey = 'your-reef-pi-api-key';
 
 const api = axios.create({
@@ -10,6 +10,42 @@ const api = axios.create({
   }
 });
 
+//temperatur probe requests
+
+// set the base URL of the reef-pi API
+const reefPiApiBaseUrl = 'http://<your-reef-pi-ip-address>/api';
+
+// set the ID of the temperature probe you want to read
+const temperatureProbeId = '<your-temperature-probe-id>';
+
+// set the endpoint to get the current temperature reading for the specified probe
+const endpoint = `/api/probes/${temperatureProbeId}/current_reading`;
+
+// make the GET request using Axios
+axios.get(`${reefPiApiBaseUrl}${endpoint}`)
+  .then(response => {
+    // handle the response data
+    console.log(`Current temperature reading for probe ${temperatureProbeId}: ${response.data.value}`);
+  })
+  .catch(error => {
+    // handle errors
+    console.error(error);
+  });
+
+//pH probe requests
+
+// Replace 'http://192.168.1.100:8080' with the IP address and port number of your reef-pi instance.
+const endpoint = 'http://192.168.1.100:8080/api/phprobes/1/readings';
+
+axios.get(endpoint)
+  .then(response => {
+    // The response contains the pH readings as an array of objects.
+    const phData = response.data;
+    console.log(phData);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 export default {
   // pH Probes
   getAllPhProbes: () => api.get('/ph_probes'),
