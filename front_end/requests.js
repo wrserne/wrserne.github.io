9ca54b6acac56c0e6dfc7af2d4939c55b75d80a2
Will Serne <wrserne@gmail.com>
2023-02-27 09:44:35 -0500

    update

--- front_end/requests.js
+++ front_end/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://your-reef-pi-api-url/'; // this should be my rasberry pi IP on local network
+const baseURL = 'http://your-reef-pi-api-url/';
 const apiKey = 'your-reef-pi-api-key';
 
 const api = axios.create({
@@ -10,58 +10,26 @@ const api = axios.create({
   }
 });
 
-//temperatur probe requests
-
-// set the base URL of the reef-pi API
-const reefPiApiBaseUrl = 'http://<your-reef-pi-ip-address>/api';
-
-// set the ID of the temperature probe you want to read
+// Temperature probe request
 const temperatureProbeId = '<your-temperature-probe-id>';
-
-// set the endpoint to get the current temperature reading for the specified probe
-const endpoint = `/api/probes/${temperatureProbeId}/current_reading`;
-
-// make the GET request using Axios
-axios.get(`${reefPiApiBaseUrl}${endpoint}`)
+const temperatureEndpoint = `/api/probes/${temperatureProbeId}/current_reading`;
+api.get(temperatureEndpoint)
   .then(response => {
-    // handle the response data
     console.log(`Current temperature reading for probe ${temperatureProbeId}: ${response.data.value}`);
   })
   .catch(error => {
-    // handle errors
     console.error(error);
   });
 
-//pH probe requests
-
-// Replace 'http://192.168.1.100:8080' with the IP address and port number of your reef-pi instance.
-const endpoint = 'http://192.168.1.100:8080/api/phprobes/1/readings';
-
-axios.get(endpoint)
+// pH probe request
+const phEndpoint = '/api/phprobes/1/readings'; // Replace '1' with the ID of your pH probe
+api.get(phEndpoint, { baseURL })
   .then(response => {
-    // The response contains the pH readings as an array of objects.
     const phData = response.data;
     console.log(phData);
   })
   .catch(error => {
     console.error(error);
   });
-export default {
-  // pH Probes
-  getAllPhProbes: () => api.get('/ph_probes'),
-  getPhProbe: (id) => api.get(`/ph_probes/${id}`),
-  createPhProbe: (data) => api.post('/ph_probes', data),
-  updatePhProbe: (id, data) => api.put(`/ph_probes/${id}`, data),
-  deletePhProbe: (id) => api.delete(`/ph_probes/${id}`),
 
-  // Temperature
-  getAllTemperatureProbes: () => api.get('/temperature_probes'),
-  getTemperatureProbe: (id) => api.get(`/temperature_probes/${id}`),
-  createTemperatureProbe: (data) => api.post('/temperature_probes', data),
-  updateTemperatureProbe: (id, data) => api.put(`/temperature_probes/${id}`, data),
-  deleteTemperatureProbe: (id) => api.delete(`/temperature_probes/${id}`),
-  getCurrentTemperatureReading: (id) => api.get(`/temperature_probes/${id}/current_reading`),
-  
-  // General
-  login: (data) => api.post('/auth/signin', data)
-};
+export default api;
